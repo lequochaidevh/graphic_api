54ab3ac93325831eefe232f0a3bb8c593727d11e
lequochaidevh <lequochaidevh@gmail.com>
2026-02-08 22:41:17 +0700

    Move logo C with imgui debug

--- OpenGL_helper/src/Application.cpp
+++ OpenGL_helper/src/Application.cpp
@@ -85,20 +85,9 @@ int main() {
         glm::ortho(0.0f, 960.0f, 0.0f, 540.0f,  //
                    -1.0f, 1.0f);                // calib width:height 4:3
 
-    glm::mat4 view = glm::translate(
-        glm::mat4(1.0f),
-        glm::vec3(500.0f, 0.0f,
-                  0.0f));  // move camera to left object will move right
-
-    glm::mat4 model = glm::translate(
-        glm::mat4(1.0f), glm::vec3(-300.0f, 0.0f, 0.0f));  // move model left
-
-    glm::mat4 mvp = proj * view * model;  // model view projection
-
     Shader shader("res/shaders/BasicTexture.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.0f, 0.3f, 0.8f, 1.0f);
-    shader.SetUniformMat4f("u_MVP", mvp);
 
     Texture texture("res/textures/C_nobgr.png");
     texture.Bind();                       // slot = 0 default
@@ -150,45 +139,26 @@ int main() {
     bool   show_another_window = false;
     ImVec4 clear_color         = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    glm::vec3 translation(-300.0f, 0.0f, 0.0f);
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = glfwGetTime();
         float deltaTime    = currentFrame - lastFrame;
         lastFrame          = currentFrame;
         renderer.Clear(colorValue);
 
+        glm::mat4 view = glm::translate(
+            glm::mat4(1.0f),
+            glm::vec3(500.0f, 0.0f,
+                      0.0f));  // move camera to left object will move right
+
+        glm::mat4 model =
+            glm::translate(glm::mat4(1.0f), translation);  // move model left
+
+        glm::mat4 mvp = proj * view * model;  // model view projection
+
+        shader.SetUniformMat4f("u_MVP", mvp);
+
         ImGui_ImplGlfwGL3_NewFrame();
-        // ImGui::NewFrame();
-        // 1. Show a simple window.
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
-        // automatically appears in a window called "Debug".
-        {
-            static float f       = 0.0f;
-            static int   counter = 0;
-            ImGui::Text("Hello, world!");  // Display some text (you can use a
-                                           // format string too)
-            ImGui::SliderFloat(
-                "float", &f, 0.0f,
-                1.0f);  // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3(
-                "clear color",
-                (float *)&clear_color);  // Edit 3 floats representing a color
-
-            ImGui::Checkbox("Demo Window",
-                            &show_demo_window);  // Edit bools storing our
-                                                 // windows open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            if (ImGui::Button(
-                    "Button"))  // Buttons return true when clicked (NB: most
-                                // widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                        1000.0f / ImGui::GetIO().Framerate,
-                        ImGui::GetIO().Framerate);
-        }
 
         renderer.Draw(VAO, EBO, shader);
         if (r > 1.0f)
@@ -201,6 +171,17 @@ int main() {
 
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.3f, 0.8f, 1.0f - colorValue, 1.0f);
+
+        // 1. Show a simple window.
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets
+        // automatically appears in a window called "Debug".
+        {
+            ImGui::SliderFloat3("Translation", &translation.x, -300.0f, 300.0f);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                        1000.0f / ImGui::GetIO().Framerate,
+                        ImGui::GetIO().Framerate);
+        }
 
         ImGui::Render();
         ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
